fd12ee51f9f90365542f573349359e6ff2421493
Himanshu Bagaria <bagaria.hb@gmail.com>
2018-06-11 23:35:45 +0530

    html loaded

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Job(models.Model):
+    period = models.CharField(max_length=200)
+    position = models.CharField(max_length=200)
+    company = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)

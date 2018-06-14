073151a131dfc44469668ce2a76c88399f08e5c0
Himanshu Bagaria <bagaria.hb@gmail.com>
2018-06-14 19:41:17 +0530

    all done

--- jobs/models.py
+++ jobs/models.py
@@ -4,4 +4,4 @@ class Job(models.Model):
     period = models.CharField(max_length=200)
     position = models.CharField(max_length=200)
     company = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
+    description = models.TextField(max_length=200)
